985a7e617602bddb96b1a0f31bfb06e347ccca27
Tsuyoshi Akiyama <ka090824@gmail.com>
2021-06-14 19:12:41 +0900

    feat: add spatial binned color feature extractuib

--- helper.py
+++ helper.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import cv2
 
 
 def plot_images(imgs, columns=2, figsize=(25, 10)):
@@ -20,3 +21,21 @@ def color_hist(img, nbins=32, bins_range=(0, 256)):
     hist_features = np.concatenate((rhist[0], ghist[0], bhist[0]))
 
     return rhist, ghist, bhist, bin_centers, hist_features
+
+
+def bin_spatial(img, color_space='RGB', size=(32, 32)):
+    if color_space != 'RGB':
+        if color_space == 'HSV':
+            feature_img = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+        elif color_space == 'LUV':
+            feature_img = cv2.cvtColor(img, cv2.COLOR_RGB2LUV)
+        elif color_space == 'HLS':
+            feature_img = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+        elif color_space == 'YUV':
+            feature_img = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+        elif color_space == 'YCrCb':
+            feature_img = cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)
+    else:
+        feature_img = np.copy(img)
+
+    return cv2.resize(feature_img, size).ravel()
