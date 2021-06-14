c714e8617b9d75d6f7ea1e8425df554a3cf34f37
Tsuyoshi Akiyama <ka090824@gmail.com>
2021-06-14 18:42:20 +0900

    feat: add color histgram helper function

--- helper.py
+++ helper.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def plot_images(imgs, columns=2, figsize=(25, 10)):
@@ -6,3 +7,16 @@ def plot_images(imgs, columns=2, figsize=(25, 10)):
     for i, img in enumerate(imgs):
         plt.subplot(len(imgs) / columns + 1, columns, i + 1)
         plt.imshow(img)
+
+
+def color_hist(img, nbins=32, bins_range=(0, 256)):
+    rhist = np.histogram(img[:, :, 0], bins=nbins, range=bins_range)
+    ghist = np.histogram(img[:, :, 1], bins=nbins, range=bins_range)
+    bhist = np.histogram(img[:, :, 2], bins=nbins, range=bins_range)
+
+    bins_edges = rhist[1]
+    bin_centers = (bins_edges[1:] + bins_edges[0: len(bins_edges) - 1]) / 2
+
+    hist_features = np.concatenate((rhist[0], ghist[0], bhist[0]))
+
+    return rhist, ghist, bhist, bin_centers, hist_features
